463be05af469719c487c35d059f53464b2407666
Rosie <sp4mme@abv.bg>
2016-11-22 13:30:25 +0200

    - Basic Car Movement. - This was done by Abood and I'm just committing it.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,52 +5,98 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Input;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture backGround;
-	Texture wellcome;
-	Texture userCar;
-	enum gamestate {
-		WelcomePage,
-		GamePage,
-		GameOver,
-	}
-	float userCarPositionX=200;
-	int choice = 0 ;
-	
-	@Override
-	public void create () {
+    SpriteBatch batch;
+    Texture backGround;
+    Texture wellcome;
+    Texture userCar;
+    Gamestate gamestate = Gamestate.WelcomePage;
+    float carSpeed = 50.0f;
+    float userCarPositionX = 450f;
+    float userCarPositionY = 620f;
+    float width = 100;
+    float height   = 70;
 
 
-		wellcome = new Texture("WellComePage.jpg") ;
-		batch = new SpriteBatch();
-		backGround=new Texture("BackGround.jpg");
-		userCar=new Texture("userCar1.png");
+    private enum Gamestate {
+        WelcomePage,
+        GamePage,
+        GameOver
+    }
 
 
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		switch (gamestate)
-		{
-			case
-		}
-		batch.draw(wellcome, 0, 0);
-		//batch.draw(backGround, 0, 0);
+    @Override
+    public void create () {
 
-		//batch.draw(userCar, userCarPositionX, 600, 100, 70 );
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		backGround.dispose();
-		wellcome.dispose();
-	}
+
+        wellcome = new Texture("WellComePage.jpg") ;
+        batch = new SpriteBatch();
+        backGround=new Texture("BackGround.jpg");
+        userCar=new Texture("userCar1.png");
+
+
+    }
+
+    @Override
+    public void render () {
+        //Gdx.gl.glClearColor(1, 0, 0, 1);
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+
+        switch (gamestate)
+        {
+            case WelcomePage:
+
+                batch.draw(wellcome, 0, 0);
+                if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+                    gamestate = Gamestate.GamePage;
+                }
+                if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+                    gamestate = Gamestate.GameOver;
+                }
+                break;
+
+            case GamePage:
+
+                batch.draw(backGround, 0, 0);
+                batch.draw(userCar, userCarPositionX, userCarPositionY , width, height  );
+                if(Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT))
+                    userCarPositionX -= Gdx.graphics.getDeltaTime() * carSpeed;
+                if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT))
+                    userCarPositionX += Gdx.graphics.getDeltaTime() * carSpeed;
+                if(Gdx.input.isKeyPressed(Input.Keys.DPAD_UP))
+                    userCarPositionY += Gdx.graphics.getDeltaTime() * carSpeed;
+
+                if(Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN))
+                    userCarPositionY -= Gdx.graphics.getDeltaTime() * carSpeed;
+
+                if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
+                {
+                    gamestate = Gamestate.GameOver;
+                }
+                //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+                //batch.begin();
+                //batch.draw(userCar, (int)userCarPositionX, (int)userCarPositionY);
+                //gamestate = Gamestate.GameOver;
+                break;
+
+            case GameOver:
+                Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+                break;
+
+        }
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        backGround.dispose();
+        wellcome.dispose();
+    }
 }
