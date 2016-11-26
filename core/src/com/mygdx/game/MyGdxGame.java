624db637d7987594456c790dba4376a4e6dc9944
Abdulrahman Alassadi <alassadi@live.com>
2016-11-26 18:39:26 +0100

    code cleanup

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -86,59 +86,78 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		batch.begin();
+
 
 		switch (gamestate)
 		{
 			case WelcomePage:
-
-				Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-				batch.draw(wellcome, 0, 0);
-				// if enter key is pressed in the welcome page it will go to game page
-				if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
-					gamestate = Gamestate.GamePage;
-				}
-				// if escape key was pressed in the welcome page it will got to the game over page
-				if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-					gamestate = Gamestate.GameOver;
-				}
+				renderWelcomePage();
 				break;
 
 			case GamePage:
+				renderGamePage();
 
-				batch.draw(backGround, 0, 0);
-				checkInput();
-				userCar.getSprite().draw(batch);
-				userCar.updatePosition();
-				aiCar.getSprite().draw(batch);
-				aiCar.updatePositionFromSpeed();
-				aiCar.Route();
-
-				// exit game
-				if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
-				{
-					Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-					gamestate = Gamestate.GameOver;
-				}
 				break;
 
 			case GameOver:
-				Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-				batch.draw(gameover, 450, 290);
-				createUserCar();
-				createAiCar();
-				if(Gdx.input.isKeyPressed(Input.Keys.SPACE))
-				{
-					Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-					gamestate = Gamestate.WelcomePage;
-				}
+				renderGameOver();
 				break;
 
 		}
+	}
 
+	public void renderWelcomePage()
+	{
+		batch.begin();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.draw(wellcome, 0, 0);
+		// if enter key is pressed in the welcome page it will go to game page
+		if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+			gamestate = Gamestate.GamePage;
+		}
+		// if escape key was pressed in the welcome page it will got to the game over page
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+			gamestate = Gamestate.GameOver;
+		}
 		batch.end();
 	}
-	
+
+
+	public void renderGamePage()
+	{
+		checkInput();
+		batch.begin();
+		batch.draw(backGround, 0, 0);
+		userCar.getSprite().draw(batch);
+		userCar.updatePosition();
+		aiCar.getSprite().draw(batch);
+		aiCar.updatePositionFromSpeed();
+		aiCar.Route();
+
+		// exit game
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
+		{
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			gamestate = Gamestate.GameOver;
+		}
+		batch.end();
+	}
+
+	public void renderGameOver()
+	{
+		batch.begin();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.draw(gameover, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		createUserCar();
+		createAiCar();
+		if(Gdx.input.isKeyPressed(Input.Keys.SPACE))
+		{
+			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			gamestate = Gamestate.WelcomePage;
+		}
+		batch.end();
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
